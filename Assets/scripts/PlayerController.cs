0827786828e2282c9d3816a0b9a0b8e93bcd3281
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2025-08-06 11:02:08 +0530

    Add project files.

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+// This script controls the player movement in a Unity game using Rigidbody physics.
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10f; 
+    private Rigidbody PlayerMax;// Speed of the player movement
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PlayerMax = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        PlayerMax.AddForce(Vector3.right * speed * HorizontalInput);
+        PlayerMax.AddForce(Vector3.forward * speed * VerticalInput);
+    }
+}
